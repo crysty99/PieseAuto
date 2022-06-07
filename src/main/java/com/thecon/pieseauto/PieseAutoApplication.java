bc96d28ae6b37f0245d3cf15ea1669dd8a7447c6
crysty99 <crysty99gl2@yahoo.com>
2022-06-07 20:49:12 +0300

    test

---
+++ src/main/java/com/thecon/pieseauto/PieseAutoApplication.java
@@ -0,0 +1,13 @@
+package com.thecon.pieseauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieseAutoApplication {
+////test
+    public static void main(String[] args) {
+        SpringApplication.run(PieseAutoApplication.class, args);
+    }
+
+}
