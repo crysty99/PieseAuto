1c7defb98042db6ec3e1795b1fcaf7f5a40a4822
crysty99 <crysty99gl2@yahoo.com>
2022-06-09 00:08:08 +0300

    tests user

---
+++ src/test/java/com/thecon/pieseauto/UserRepositoryTests.java
@@ -0,0 +1,75 @@
+package com.thecon.pieseauto;
+
+import com.thecon.pieseauto.user.User;
+import com.thecon.pieseauto.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Date;
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNewUser(){
+        User user = new User();
+        user.setName("user");
+        user.setPassword("user");
+        user.setEmail("user@gmail.com");
+        user.setDateOfBirth(new Date(2000,01, 01));
+        user.setPhoneNumber("0720000000");
+        //user.setProfileImage("/poza.png");
+        user.setActive(true);
+
+        User savedUser= repo.save(user);
+
+        Assertions.assertNotNull(savedUser);
+    }
+
+    @Test
+    public void testListAll(){
+        Iterable<User> users = repo.findAll();
+        Assertions.assertNotNull(users);
+
+        for (User user : users){
+            System.out.println(users);
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        int idUser = 1;
+        Optional<User> optionalUser = repo.findById(idUser);
+        User user = optionalUser.get();
+        user.setPhoneNumber("000000");
+        repo.save(user);
+
+        User updatedUser= repo.findById(idUser).get();
+        Assertions.assertEquals(updatedUser.getPhoneNumber(),"000000");
+    }
+
+    @Test
+    public void testGet(){
+        int idUser = 1;
+        Optional<User> optionalUser = repo.findById(idUser);
+        Assertions.assertTrue(optionalUser.isPresent());
+        System.out.println(optionalUser.get());
+    }
+
+    //@Test
+    public void testDelete(){
+        int idUser = 1;
+        repo.deleteById(idUser);
+
+        Optional<User> optionalUser = repo.findById(idUser);
+        Assertions.assertFalse(optionalUser.isPresent());
+    }
+}
